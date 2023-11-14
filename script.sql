d38dad94aa6e284ce7dc6dd050e9185ec61ba31f
Joabe Oliveira <joabefatec01bd@outlook.com>
2023-11-14 10:29:55 -0300

    feat(tb): logs

--- script.sql
+++ script.sql
@@ -1,7 +1,15 @@
+-- -- Exercicio 4
+CREATE TABLE log(
+	cod_log SERIAL PRIMARY KEY,
+	nome_youtuber varchar(200),
+	categoria VARCHAR(200),
+	inicio INT	
+);
+
 -- -- Exercicio 3
-ALTER TABLE tb_top_youtubers ADD COLUMN ativo INTEGER DEFAULT 1 CHECK (ativo IN (0, 1));
---testando
-INSERT INTO tb_top_youtubers(ativo) VALUES (2)
+-- ALTER TABLE tb_top_youtubers ADD COLUMN ativo INTEGER DEFAULT 1 CHECK (ativo IN (0, 1));
+-- --testando
+-- INSERT INTO tb_top_youtubers(ativo) VALUES (2)
 
 -- 2 Trigger
 -- Escreva um trigger com as seguintes características
